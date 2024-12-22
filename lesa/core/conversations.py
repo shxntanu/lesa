496a7b9b8b15db60881fb740fc0fe1ae052fabb6
Shantanu <shantanuwable2003@gmail.com>
2024-12-22 12:29:11 +0530

    refactor: rename conversation_manager.py to conversations.py and update related imports

--- lesa/core/conversations.py
+++ lesa/core/conversations.py
@@ -5,19 +5,16 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
 from rich.prompt import Prompt
-from rich.spinner import Spinner
 from typing import Optional, List, Union
 
-from langchain.chains import RetrievalQA
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
-from lesa.core.document_manager import DocumentManager
-from lesa.core.ollama_manager import OllamaManager
+from lesa.core.documents import DocumentManager
+from lesa.core.ollama import OllamaManager
+from lesa.utils.directory_manager import DirectoryManager
 
 class ConversationManager:
     
@@ -25,10 +22,8 @@ class ConversationManager:
     
     def __init__(self, 
                  base_path: str = '.',
-                #  output_dir: str = '.', 
                  document_model: str = "sentence-transformers/all-MiniLM-L6-v2",
-                 generator_model: str = "llama3.1:latest",
-                 generator_url: str = "http://localhost:11434"):
+    ):
         """
         Initialize the RAG pipeline with configurable parameters.
         
@@ -43,38 +38,14 @@ class ConversationManager:
         self.embedding_path = os.path.join(self.base_path, self.CONFIG_DIR, 'embeddings')
         self.document_manager = DocumentManager()
         self.ollama_manager = OllamaManager()
+        self.embeddings_manager = DirectoryManager(document_model=document_model)
         
         # Console for rich output
         self.console = Console()
     
     def _setup_preprocessing_pipeline(self):
         """Set up the preprocessing pipeline connections."""
-        components = {
-            "file_type_router": self.file_type_router,
-            "text_file_converter": self.text_file_converter,
-            "markdown_converter": self.markdown_converter,
-            "pypdf_converter": self.pdf_converter,
-            "document_joiner": self.document_joiner,
-            "document_cleaner": self.document_cleaner,
-            "document_splitter": self.document_splitter,
-            "document_embedder": self.document_embedder,
-            "document_writer": self.document_writer
-        }
-        
-        for name, component in components.items():
-            self.preprocessing_pipeline.add_component(instance=component, name=name)
-        
-        # Pipeline connections
-        self.preprocessing_pipeline.connect("file_type_router.text/plain", "text_file_converter.sources")
-        self.preprocessing_pipeline.connect("file_type_router.application/pdf", "pypdf_converter.sources")
-        self.preprocessing_pipeline.connect("file_type_router.text/markdown", "markdown_converter.sources")
-        self.preprocessing_pipeline.connect("text_file_converter", "document_joiner")
-        self.preprocessing_pipeline.connect("pypdf_converter", "document_joiner")
-        self.preprocessing_pipeline.connect("markdown_converter", "document_joiner")
-        self.preprocessing_pipeline.connect("document_joiner", "document_cleaner")
-        self.preprocessing_pipeline.connect("document_cleaner", "document_splitter")
-        self.preprocessing_pipeline.connect("document_splitter", "document_embedder")
-        self.preprocessing_pipeline.connect("document_embedder", "document_writer")
+        pass
     
     def _setup_conversation_pipeline(self, document_model: str):
         """Set up the conversation pipeline connections."""
@@ -164,23 +135,10 @@ class ConversationManager:
         """
         
         documents = self.document_manager.extract_document_text(file_path)
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
-        docs = text_splitter.split_documents(documents=documents)
+        docs = self.embeddings_manager.text_splitter.split_documents(documents=documents)
         
-        embeddings = HuggingFaceEmbeddings(
-            model_name=embedding_model,
-        )
-
-        # Create FAISS vector store
-        vectorstore = FAISS.from_documents(docs, embeddings)
-
-        # Save and reload the vector store
-        vectorstore.save_local(f"{self.CONFIG_DIR}/embeddings")
-        persisted_vectorstore = FAISS.load_local(
-            folder_path=f"{self.CONFIG_DIR}/embeddings", 
-            embeddings=embeddings, 
-            allow_dangerous_deserialization=True
-        )
+        # Embed documents
+        self.embeddings_manager.embed_documents(docs)
         
         llm = self.ollama_manager.serve_llm()
         
@@ -194,7 +152,7 @@ class ConversationManager:
         ])
         
         combine_docs_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
-        qa_chain = create_retrieval_chain(retriever=persisted_vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain)
+        qa_chain = create_retrieval_chain(retriever=self.embeddings_manager.vector_store.as_retriever(), combine_docs_chain=combine_docs_chain)
         return self._chat(qa_chain)
     
     def start_conversation(self):
